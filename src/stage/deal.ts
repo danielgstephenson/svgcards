dc5d9f96f3ffc633264968c482b3ad77e0f722ce
David Y. Stephenson <david@davidystephenson.com>
2025-05-31 15:01:49 -0400

    Tweaked cards

--- src/stage/deal.ts
+++ src/stage/deal.ts
@@ -14,7 +14,7 @@ export class Deal {
     const cards = setupMessage.cards
     console.log('cards', cards)
     const ids = [...cards.keys()]
-    const guaranteedRanks: number[] = [8, 4]
+    const guaranteedRanks: number[] = [8, 4, 12, 13, 14, 15]
     const guaranteedIndices = guaranteedRanks.map((rank) => rank - 1)
     const shuffleable = ids.filter(i => i !== 4 && i !== 0 && !guaranteedIndices.includes(i))
     const shuffled = stage.shuffle(shuffleable)
