1c3520684576b40044a972e9fd2bdb406d62c99b
David Y. Stephenson <david@davidystephenson.com>
2025-09-16 14:06:54 -0400

    Deal warrior

--- src/stage/deal.ts
+++ src/stage/deal.ts
@@ -14,7 +14,7 @@ export class Deal {
     const cards = setupMessage.cards
     console.info('cards', cards)
     const ids = [...cards.keys()]
-    const guaranteedRanks: number[] = [2, 7, 11]
+    const guaranteedRanks: number[] = [2, 7, 9, 11]
     console.info('guaranteedRanks', guaranteedRanks)
     const excludedRanks: number[] = []
     const guaranteedIndices = guaranteedRanks.map((rank) => rank - 1)
